263af74a1c06ccde8fdb3d78f9ca812df5e07b60
mnaibei <muchajulius@gmail.com>
2024-02-16 18:01:29 +0300

    Styling

--- pages/resident-details/[id].tsx
+++ pages/resident-details/[id].tsx
@@ -30,6 +30,8 @@ const ResidentDetailsPage = () => {
     }
   }, [router.query]);
 
+  console.log(resident);
+
   const handleNotesChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
     setNotes(event.target.value);
   };
@@ -53,25 +55,31 @@ const ResidentDetailsPage = () => {
   }
 
   return (
-    <div className=" flex flex-col ">
-      <div className="border-2 border-yellow-500 h-20 w-3/4 pl-3 pr-3 self-center mb-5">
+    <div className=" flex flex-col h-full ">
+      <div className=" h-20 w-3/4 pl-3 pr-3 self-center mb-5 max-sm:w-full">
         <Nav />
       </div>
-      <div className="border-2 border-red-500  grid grid-cols-2 w-3/4 self-center gap-4 ">
+      <div className="  grid grid-cols-2 w-3/4 h-full self-center gap-4 max-sm:grid-cols-1 max-sm:w-fit">
         <div className="mt-5 border-3 flex flex-col items-center">
-          <h2 className="mb-2">Resident Details</h2>
-          <img src={resident.image} alt={resident.name} />
-          <p>Name: {resident.name}</p>
-          <p>Status: {resident.status}</p>
-          <p>Species: {resident.species}</p>
-          <p>Gender: {resident.gender}</p>
-          <p>Location: {resident.location.name}</p>
-          <p>Origin: {resident.origin.name}</p>
+          <h2 className="mb-2 font-extrabold">Resident Details</h2>
+          <img
+            src={resident.image}
+            alt={resident.name}
+            className="border-2 border-green-500"
+          />
+          <div className="info mt-2 grid grid-cols-2 gap-x-4 p-2 max-sm:gap-x-2">
+            <p>Name: {resident.name}</p>
+            <p>Status: {resident.status}</p>
+            <p>Species: {resident.species}</p>
+            <p>Gender: {resident.gender}</p>
+            <p>Location: {resident.location.name}</p>
+            <p>Origin: {resident.origin.name}</p>
+          </div>
         </div>
-        <div className="note-section border-2 flex flex-col gap-4 border-green-500">
+        <div className="note-section flex flex-col gap-4 p-2 justify-center">
           <div>
             <h3>Previous Notes:</h3>
-            <div className=" flex flex-col items-left mt-3 border-2 border-blue-500">
+            <div className=" flex flex-col items-left mt-3">
               <ul>
                 {allNotes.map((note, index) => (
                   <li key={index}>{note}</li>
@@ -85,11 +93,12 @@ const ResidentDetailsPage = () => {
               id="notes"
               value={notes}
               onChange={handleNotesChange}
+              placeholder="Add a note..."
               className="w-full border-2 border-gray-400 rounded p-2 mt-2"
             />
             <button
               type="submit"
-              className="bg-blue-500 text-white px-4 py-2 mt-2">
+              className="bg-blue-500 text-white px-4 py-2 mt-2 mb-10">
               Save Note
             </button>
           </form>
